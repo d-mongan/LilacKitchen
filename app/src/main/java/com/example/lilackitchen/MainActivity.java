c821848e7f9741591f6ed84400b2d87938aa37c2
d-mongan <darren_m16@hotmail.com>
2022-10-26 16:54:42 +1300

    Empty template with swipe functionality

--- app/src/main/java/com/example/lilackitchen/MainActivity.java
+++ app/src/main/java/com/example/lilackitchen/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.lilackitchen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.viewPager2);
+
+    }
+
+    public void setViewPagerAdapter() {
+        ViewPager2Adapter viewPager2Adapter = new
+                ViewPager2Adapter(this);
+        ArrayList<Fragment> fragmentList = new ArrayList<>(); //creates an ArrayList of Fragments
+        fragmentList.add(new RecipesFragment());
+        fragmentList.add(new POSFragment());
+        fragmentList.add(new ReportsFragment());
+        viewPager2Adapter.setData(fragmentList); //sets the data for the adapter
+        viewPager2.setAdapter(viewPager2Adapter);
+
     }
 }
